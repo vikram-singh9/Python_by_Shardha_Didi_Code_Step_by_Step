0abdf4a218dabf213a067311a0a56dd0f217b2bb
vikram-singh9 <163000375+vikram-singh9@users.noreply.github.com>
2025-03-04 22:55:45 +0500

    updated

---
+++ lecture05-loops-in-python/loop.py
@@ -0,0 +1,10 @@
+# two types of loops 1 for loop and 2 while loop
+
+# while loop
+count = 1
+while count <= 10:
+    print("vikram")
+    count += 1
+
+# iteration loop ka chakar lagana (1 iteration means ek baar loop ka chakar lagaya!) 
+# iterator means jis variable ka use use kya chakr lagane k lye
