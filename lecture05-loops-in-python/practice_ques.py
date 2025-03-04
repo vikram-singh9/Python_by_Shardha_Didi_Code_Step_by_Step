9025e9bb555ce06316593f08b7961a9ad4431e2c
vikram-singh9 <163000375+vikram-singh9@users.noreply.github.com>
2025-03-04 23:33:22 +0500

    Update practice_ques.py

---
+++ lecture05-loops-in-python/practice_ques.py
@@ -0,0 +1,17 @@
+# print numbers from 1 to 100 using while loop
+# very easy let's start coding!
+number = 1
+while number <= 99:
+    number += 1
+    print(number)
+
+# reverse order 
+
+n = 100
+while n >= 1: # this is the stopping condition
+    n -= 1
+    print(n)
+
+
+# print the multiplication table of number n?
+
