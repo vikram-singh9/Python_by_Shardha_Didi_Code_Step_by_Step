fabc7ff3326ed89e3ce9b33b713aae964c618344
vikram-singh9 <163000375+vikram-singh9@users.noreply.github.com>
2025-03-05 00:09:52 +0500

    Update practice_ques.py

--- lecture05-loops-in-python/practice_ques.py
+++ lecture05-loops-in-python/practice_ques.py
@@ -15,3 +15,28 @@ while n >= 1: # this is the stopping condition
 
 # print the multiplication table of number n?
 
+n = 1 
+while n <= 9:
+    n += 1
+    print(n*3)
+
+# break and continue in while loop!
+# BREAK used to terminate the loop when encountered.
+
+m = 1
+while m <= 5:
+    print(m)
+    if(m==3): # 3 pe break karega aage nhn chlega 
+        break
+    m += 1
+
+# CONTINUE
+# terminates execution in the current iteration and continues excution of the loop with the next iteration.
+
+o = 0
+while o <= 10:
+    if(o==6): # 6 print nhn hoga!
+        o+=1
+        continue # skip in the current iteration (skip karo uske bad aage chalao simple)
+    print(o)
+    o += 1
